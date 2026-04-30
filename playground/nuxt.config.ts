320d847b1635befe0c6092d3ab9cb5995f2a303e
Valbert Vieira <programador02sundownpark@gmail.com>
2026-04-30 15:58:28 -0300

    chore: remove leaked playground client id, add .env.example

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -4,6 +4,6 @@ export default defineNuxtConfig({
   compatibilityDate: 'latest',
   paypal: {
     components: ['buttons', 'marks', 'messages'],
-    clientId: 'REDACTED',
+    clientId: '',
   },
 })
